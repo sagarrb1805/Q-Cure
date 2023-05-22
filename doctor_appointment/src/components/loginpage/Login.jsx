9a2462f2374f71f1952639ca541188f289d5472c
Sagar R.B <sagarrb1805@gmail.com>
2023-05-22 12:55:25 +0530

    styles in login page

--- doctor_appointment/src/components/loginpage/Login.jsx
+++ doctor_appointment/src/components/loginpage/Login.jsx
@@ -1,17 +1,20 @@
 import Popup from 'reactjs-popup'
 import SelectionType from './selectionType/SelectionType'
+import './login.css'
 const Login = () => {
    
 
   return (
-    <div>
+    <div className='parent-div'>
         <h2>Welcome</h2>
 
-        <form>
-            <input type='email' placeholder='Email'/><br />
-            <input type='password' placeholder='Password'/><br />
-            <input type='submit' value='Signin'/>
-        </form>
+        <div className='form-div'>
+            <form className='login-form'>
+                <input type='email' placeholder='Email'/><br />
+                <input type='password' placeholder='Password'/><br />
+                <input type='submit' value='Signin'/>
+            </form>
+        </div>
 
        
         <Popup trigger={<p>Don't have an account <a href="#">Register</a> yourself</p>}>
