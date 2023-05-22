ca14fc6eb8dba49da7ac2e510c6fda76dfe519a2
Sagar R.B <sagarrb1805@gmail.com>
2023-05-22 10:07:07 +0530

    Login form

--- doctor_appointment/src/components/loginpage/Login.jsx
+++ doctor_appointment/src/components/loginpage/Login.jsx
@@ -5,8 +5,13 @@ const Login = () => {
 
   return (
     <div>
-        <h2>Login Page</h2>
+        <h2>Welcome</h2>
 
+        <form>
+            <input type='email' placeholder='Email'/><br />
+            <input type='password' placeholder='Password'/><br />
+            <input type='submit' value='Signin'/>
+        </form>
 
        
         <Popup trigger={<p>Don't have an account <a href="#">Register</a> yourself</p>}>
