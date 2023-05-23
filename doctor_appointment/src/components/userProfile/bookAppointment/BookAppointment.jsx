c19465f9622d7512bce9f9c784e10e7750180e46
Sagar R.B <sagarrb1805@gmail.com>
2023-05-23 12:48:07 +0530

    book appointment basic form

--- doctor_appointment/src/components/userProfile/bookAppointment/BookAppointment.jsx
+++ doctor_appointment/src/components/userProfile/bookAppointment/BookAppointment.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function BookAppointment() {
   return (
-    <div>BookAppointment</div>
+    <div>
+    <div>
+      <h3>Book Your Slot</h3>
+      </div>
+    <div>
+      <input type="datetime-local" id="meeting-time" name="meeting-time" />
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="text" placeholder="Ente Your Blood Group" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="text" placeholder="Enter Your Weight" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="text" placeholder="Search for Doctor based on  specialization" />
+      </Form.Group>
+
+      <Button type="submit" >
+      Search
+      </Button><br />
+
+      <label for="dog-names">Choose a doctor:</label> 
+        <select name="dog-names" id="dog-names"> 
+            <option value="rigatoni">Rigatoni</option> 
+            <option value="dave">Dave</option> 
+            <option value="pumpernickel">Pumpernickel</option> 
+            <option value="reeses">Reeses</option> 
+        </select>
+        <br />
+        <Button type="submit" >
+      Submit
+      </Button>
+
+
+    </div>
+    </div>
   )
 }
 
